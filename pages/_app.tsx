7af15b9a56ae3e320742e38c4abc3c28f01804d5
zhanghanzhe666 <146498764+zhanghanzhe666@users.noreply.github.com>
2024-06-22 15:30:58 +0800

    Add files via upload

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import '../styles/globals.css';
+import 'antd/dist/antd.css';
+import '../request';
+import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import { store } from '../store/store';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Provider store={ store }>
+      <Component {...pageProps} />
+    </Provider>
+  )
+}
+
+export default MyApp
